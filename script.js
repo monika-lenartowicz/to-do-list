82448021804d223d7ef2ccabdc2861e7ef278792
Monika Lenartowicz <nika.nel@interia.pl>
2022-01-05 15:46:06 +0100

    changes in class name, adding new tasks

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-// 
+{
+   const tasks = [
+      {
+         content: "nagrać lekcję",
+         done: false,
+      },
+
+      {
+         content: "zjeść pierogi",
+         done: true,
+      },
+   ]
+
+   const render = () => {
+      let htmlString = " ";
+
+      for (const task of tasks) {
+         htmlString += `
+         <li class="taskContainer__item">
+            <button class="taskContainer__done ">✓</button>
+            <span class = "text__done">${task.content}</span>
+            <button class="taskContainer__remove">
+            🗑
+           </button>
+         </li>
+        
+           `;
+      }
+
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+
+   }
+
+   const addNewTAsk = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      })
+      render();
+
+   }
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+         return
+      }
+      addNewTAsk(newTaskContent);
+   }
+
+
+
+const init = () => {
+   render();
+
+   const form = document.querySelector(".js-form");
+   form.addEventListener("submit", onFormSubmit);
+};
+
+init();
+}
